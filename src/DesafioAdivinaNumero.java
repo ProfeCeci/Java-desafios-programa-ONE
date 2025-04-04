f68a67b520bc5361a83ce1c079f1892ed45c34be
Cecilia Lopez <andreacecilopez88@gmail.com>
2025-04-04 20:34:14 -0300

    Add challenge

---
+++ src/DesafioAdivinaNumero.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class DesafioAdivinaNumero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroAleatorio =  new Random().nextInt(100);
+        int numeroUsuario = 0;
+        int intentos = 5;
+        int i = 0;
+
+        while (i < intentos) {
+            System.out.println("Ingrese un número entre 0 y 100 para adivinar el número secreto: ");
+            numeroUsuario = scanner.nextInt();
+
+            if (numeroAleatorio == numeroUsuario){
+                System.out.println("¡Adivinaste!");
+                break;
+            } else if (numeroAleatorio > numeroUsuario) {
+                System.out.println("El número es mayor");
+            } else {
+                System.out.println("El número es menor");
+            }
+
+            i++;
+        }
+
+        if (i == intentos){
+            System.out.println("No tienes más intentos el número era " + numeroAleatorio);
+        }
+    }
+}
