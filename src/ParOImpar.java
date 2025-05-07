98919b094b7dc1bd641ea5cf10589bae6cf3f030
Cecilia Lopez <andreacecilopez88@gmail.com>
2025-05-07 18:29:36 -0300

    Add ParOImpar class to determine even or odd numbers

---
+++ src/ParOImpar.java
@@ -0,0 +1,11 @@
+public class ParOImpar {
+    public static void main(String[] args) {
+        int numero = 20;
+
+        if (numero % 2 == 0) {
+            System.out.println("El número " + numero + " es par.");
+        } else {
+            System.out.println("El número " + numero + " es impar.");
+        }
+    }
+}
